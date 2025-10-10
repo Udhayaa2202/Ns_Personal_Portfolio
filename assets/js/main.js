f474262ed200ec746e4e84308b827d5871323ca8
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2025-10-10 14:32:17 +0530

    Contact section designed for mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,8 +32,27 @@ const shadowHeader = () => {
 window.addEventListener('scroll',shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('','','','')
+    .then(() =>{
+        contactMessage.textContent = 'Message sent successfully ✅'
 
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 500)
+
+        contactForm.reset()
+    }, () => {
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit',sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
