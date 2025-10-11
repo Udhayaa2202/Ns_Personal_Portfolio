9f4f6f9924daaa88da593a56b2b8034a0a10c0b3
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2025-10-11 14:59:34 +0530

    Added Animations for all sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 
-if(navToggle){
-    navToggle.addEventListener('click',() =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-if(navClose){
-    navClose.addEventListener('click',() =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -22,59 +22,59 @@ const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== SHADOW HEADER ===============*/
 const shadowHeader = () => {
     const header = document.getElementById('header')
     this.scrollY >= 50 ? header.classList.add('shadow-header')
-                       : header.classList.remove('shadow-header');
+        : header.classList.remove('shadow-header');
 }
-window.addEventListener('scroll',shadowHeader)
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message')
+    contactMessage = document.getElementById('contact-message')
 
 const sendEmail = (e) => {
     e.preventDefault()
     // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('','','','')
-    .then(() =>{
-        contactMessage.textContent = 'Message sent successfully ✅'
+    emailjs.sendForm('', '', '', '')
+        .then(() => {
+            contactMessage.textContent = 'Message sent successfully ✅'
 
-        setTimeout(() => {
-            contactMessage.textContent = ''
-        }, 500)
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 500)
 
-        contactForm.reset()
-    }, () => {
-        contactMessage.textContent = 'Message not sent (service error) ❌'
-    })
+            contactForm.reset()
+        }, () => {
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
 }
 
-contactForm.addEventListener('submit',sendEmail)
+contactForm.addEventListener('submit', sendEmail)
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                        : scrollUp.classList.remove('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
 }
-window.addEventListener('scroll',scrollUp)
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
 const scrollActive = () => {
-    const scrollDown =  window.scrollY
+    const scrollDown = window.scrollY
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop - 58,
-          sectionId = current.getAttribute('id'),
-          sectionClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
 
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
             sectionClass.classList.add('active-link')
         } else {
             sectionClass.classList.remove('active-link')
@@ -82,8 +82,8 @@ const scrollActive = () => {
     })
 }
 
-/*=============== DARK LIGHT THEME ===============*/ 
-const themeButton =  document.getElementById('theme-button')
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
 
@@ -93,9 +93,9 @@ const selectedIcon = localStorage.getItem('selected-icon')
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => document.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
 
-if(selectedTheme){
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'] (darkTheme)
-    themeButton.classList[selectedTheme === 'ri-moon-line' ? 'add' : 'remove'] (iconTheme)
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedTheme === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
 themeButton.addEventListener('click', () => {
@@ -106,3 +106,13 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home_perfil, .about_image, .contact_mail', { origin: 'right' })
+sr.reveal('.home_name, .home_info, .about_container, .section_title-1, .about_info, .contact_social, .contact_data', {origin:'left'})
+sr.reveal('.services_card, .projects_card', { interval: 100 }) 
